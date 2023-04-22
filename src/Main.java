76e18f6ce3232566aafe7fa667f8eeda83695382
codewithmotari <bsit533j2021@students.tum.ac.ke>
2023-04-22 14:07:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int i=0;
+        while(true) {
+
+            System.out.println(i);
+            i++;
+        }
+        }
+    }
